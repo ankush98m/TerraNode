b61987a391fee02fcf6eda5847158368ffb7a23e
Ankush Maheshwari <ankush97m@gmail.com>
2024-09-25 00:40:14 -0400

    connect to the database

--- app/app.js
+++ app/app.js
@@ -1,6 +1,20 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
+const { Client } = require('pg')
+
+const client = new Client({ 
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST, 
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD, 
+    port: process.env.DB_PORT, 
+    });
+
+client.connect() 
+    .then(() => { console.log('Connected to PostgreSQL database!'); }) 
+    .catch((err) => { console.error('Error connecting to the database:', err); });
 
 app.get('/', (req, res)=>{
     res.send('Hello World!')
