7a2196b159df132fbd7a62b55fb6f1c18f3ddd1f
Ankush Maheshwari <ankush97m@gmail.com>
2024-09-25 01:21:02 -0400

    create healthz endpoint for get method

--- app/app.js
+++ app/app.js
@@ -20,6 +20,20 @@ app.get('/', (req, res)=>{
     res.send('Hello World!')
 })
 
+app.get('/healthz', async (req, res) => {
+    // api contains any payload
+    if(req.body && Object.keys(req.body).length > 0){
+        return res.status(400).set('Cache-Control', 'no-cache').send();
+    }
+
+    try{
+        await client.query('SELECT 1');
+        res.status(200).set('Cache-Control', 'no-cache').send();
+    } catch(err){
+        res.status(503).set('Cache-Control', 'no-cache').send();
+    }
+});
+
 app.listen(port, ()=>{
     console.log(`Webapp listening on port ${port}`)
 })
