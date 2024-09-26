d2eaf1f8d2e617fdf379ba4dac57f9b5bc0a7a0a
Ankush Maheshwari <ankush97m@gmail.com>
2024-09-25 23:43:56 -0400

    handle database connection error

--- app/app.js
+++ app/app.js
@@ -1,44 +1,48 @@
-require('dotenv').config();
-const express = require('express')
-const app = express()
-const port = 3000
-const { Client } = require('pg')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+const { Pool } = require("pg");
 
-const client = new Client({ 
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST, 
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD, 
-    port: process.env.DB_PORT, 
-    });
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
-client.connect() 
-    .then(() => { console.log('Connected to PostgreSQL database!'); }) 
-    .catch((err) => { console.error('Error connecting to the database:', err); });
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
-app.get('/', (req, res)=>{
-    res.send('Hello World!')
-})
-
-app.get('/healthz', async (req, res) => {
-    // api contains any payload
-    if(req.body && Object.keys(req.body).length > 0){
-        return res.status(400).set('Cache-Control', 'no-cache').send();
-    }
-
-    try{
-        await client.query('SELECT 1');
-        res.status(200).set('Cache-Control', 'no-cache').send();
-    } catch(err){
-        res.status(503).set('Cache-Control', 'no-cache').send();
-    }
+app.get("/healthz", async (req, res) => {
+  // No payload allowed
+  if (req.body && Object.keys(req.body).length > 0) {
+    return res.status(400).send();
+  }
+  try {
+    // Connect to the database
+    await pool.connect();
+    res
+      .status(200)
+      .set("Cache-Control", "no-cache", "no-store", "must-revalidate")
+      .send();
+  } catch (err) {
+    console.error("Database connection error:", err);
+    // Send 503 if the database connection fails
+    res
+      .status(503)
+      .set("Cache-Control", "no-cache", "no-store", "must-revalidate")
+      .send();
+  }
 });
 
 // Handle unsupported HTTP methods for the /healthz endpoint
-app.all('/healthz', (req, res) => {
-    res.status(405).set('Cache-Control', 'no-cache').send();
-  });
+app.all("/healthz", (req, res) => {
+  res.status(405).set("Cache-Control", "no-cache").send();
+});
 
-app.listen(port, ()=>{
-    console.log(`Webapp listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Webapp listening on port ${port}`);
+});
