428a27c378a1fdcc257e49019487d4d24111bf2e
Ankush Maheshwari <ankush97m@gmail.com>
2024-09-26 10:54:01 -0400

    use sequelize to connect to database

--- app/app.js
+++ app/app.js
@@ -2,15 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
-const { Pool } = require("pg");
+const { Sequelize, DataTypes } = require("sequelize");
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres', 
+  });
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World!");
@@ -23,7 +20,7 @@ app.get("/healthz", async (req, res) => {
   }
   try {
     // Connect to the database
-    await pool.connect();
+    await sequelize.authenticate();
     res
       .status(200)
       .set("Cache-Control", "no-cache", "no-store", "must-revalidate")
