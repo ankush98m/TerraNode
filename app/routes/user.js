8ed9b5a1f3a2114373a526bf8946079100a4fa86
Ankush Maheshwari <ankush97m@gmail.com>
2024-09-30 18:56:05 -0400

    create put api to update user info

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,10 +2,8 @@ const express = require("express");
 const UserModel = require("../models/user");
 const bcrypt = require("bcrypt");
 
-
 const router = express.Router();
 module.exports = (sequelize) => {
-  
   const User = UserModel(sequelize);
 
   // post request to add a user
@@ -17,7 +15,7 @@ module.exports = (sequelize) => {
       const isExistingUser = await User.findOne({ where: { email } });
       if (isExistingUser) {
         return res.status(400).json({
-          message: "User already exists"
+          message: "User already exists",
         });
       }
 
@@ -26,7 +24,7 @@ module.exports = (sequelize) => {
         first_name,
         last_name,
         email,
-        password
+        password,
       });
 
       // set the response
@@ -45,23 +43,28 @@ module.exports = (sequelize) => {
   });
 
   // get request to retreive user information
-  router.get('/v1/user/self', async (req, res) => {
+  router.get("/v1/user/self", async (req, res) => {
     try {
       const authHeader = req.headers.authorization;
       // check authheader exist or not
       if (!authHeader) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({ message: "Unauthorized" });
       }
 
       // extract username and password from authheader
-      const [username, password] = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      const [username, password] = Buffer.from(
+        authHeader.split(" ")[1],
+        "base64"
+      )
+        .toString()
+        .split(":");
       const user = await User.findOne({ where: { email: username } });
 
       // check password matches or not
       if (!user || !(await bcrypt.compare(password, user.password))) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({ message: "Unauthorized" });
       }
-  
+
       res.json({
         id: user.id,
         first_name: user.first_name,
@@ -72,7 +75,73 @@ module.exports = (sequelize) => {
       });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+
+  router.put("/v1/user/self", async (req, res) => {
+    try {
+        console.log("in put api")
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      // extract username and password from authheader
+      const [username, password] = Buffer.from(
+        authHeader.split(" ")[1],
+        "base64"
+      )
+        .toString()
+        .split(":");
+      const user = await User.findOne({ where: { email: username } });
+
+      // check password matches or not
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      const { first_name, last_name, password: newPassword } = req.body;
+      
+      const allowedFields = ["first_name", "last_name", "password"];
+      const invalidFields = Object.keys(req.body).filter(
+        (field) => !allowedFields.includes(field)
+      );
+
+      // return 400 for invalid fields
+      if (invalidFields.length > 0) {
+        return res.status(400).json({message: "Bad request"});
+      }
+
+      // return 204 when all fields are empty
+      if(first_name == undefined && last_name == undefined && newPassword == undefined){
+        return res.status(204).json({ message: "No content" })
+      }
+
+      // updating fields
+      if (first_name && first_name.length > 0) {
+        user.first_name = first_name;
+      }
+      if (last_name && last_name.length > 0) {
+        user.last_name = last_name;
+      }
+      if (newPassword && newPassword.length > 0) {
+        user.password = newPassword;
+      }
+
+      await user.save();
+
+      res.json({
+        id: user.id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        account_created: user.account_created,
+        account_updated: user.account_updated,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
     }
   });
 
