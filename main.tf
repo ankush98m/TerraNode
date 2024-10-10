5bf0b580ff91e4869b48b30a8a363d172d43875a
Ankush Maheshwari <ankush97m@gmail.com>
2024-10-09 22:41:01 -0400

    add private route table

--- main.tf
+++ main.tf
@@ -111,3 +111,28 @@ resource "aws_route_table_association" "public_3" {
   subnet_id      = aws_subnet.public_subnet_3.id
   route_table_id = aws_route_table.public.id
 }
+
+# Private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Private Route Table"
+  }
+}
+
+# Private subnets with the private route table
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_3" {
+  subnet_id      = aws_subnet.private_subnet_3.id
+  route_table_id = aws_route_table.private.id
+}
