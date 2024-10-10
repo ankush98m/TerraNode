f9d7775036c16c9a566253e63c6b37f771a2965f
Ankush Maheshwari <ankush97m@gmail.com>
2024-10-09 21:45:14 -0400

    create public subnets

--- main.tf
+++ main.tf
@@ -9,3 +9,33 @@ resource "aws_vpc" "main" {
     Name = "CSYE6225-VPC"
   }
 }
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  
+  tags = {
+    Name = "Public Subnet 1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  
+  tags = {
+    Name = "Public Subnet 2"
+  }
+}
+
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+  
+  tags = {
+    Name = "Public Subnet 3"
+  }
+}
