f078fca2cbc1ba63e0290afdb2db3697bd4c278f
Ankush Maheshwari <ankush97m@gmail.com>
2024-10-09 22:36:57 -0400

    add public route table

--- main.tf
+++ main.tf
@@ -80,3 +80,34 @@ resource "aws_subnet" "private_subnet_3" {
     Name = "Private Subnet 3"
   }
 }  
+
+
+# public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+# public subnets with the public route table
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_3" {
+  subnet_id      = aws_subnet.public_subnet_3.id
+  route_table_id = aws_route_table.public.id
+}
