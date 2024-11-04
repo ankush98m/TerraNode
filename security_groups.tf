74fec30cc679d46cba77a28614eb367e1c5c2691
Ankush Maheshwari <ankush97m@gmail.com>
2024-11-04 16:27:21 -0500

    add load balancer security group

--- security_groups.tf
+++ security_groups.tf
@@ -24,3 +24,24 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "load_balancer_sg" {
+  name        = "load_balancer_security_group"
+  description = "Security group for load balancer to allow HTTP and HTTPS traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
