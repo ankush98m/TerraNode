e2e8c51dd98125c38c25e5497df07b4af07eeb93
Ankush Maheshwari <ankush97m@gmail.com>
2024-11-04 16:45:04 -0500

    add autoscaling policy

--- autoscaling.tf
+++ autoscaling.tf
@@ -15,3 +15,24 @@ resource "aws_autoscaling_group" "web_app_asg" {
     propagate_at_launch = true
   }
 }
+
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "scale_up"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 60
+  autoscaling_group_name = aws_autoscaling_group.web_app_asg.name
+
+  metric_aggregation_type = "Average"
+}
+
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "scale_down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 60
+  autoscaling_group_name = aws_autoscaling_group.web_app_asg.name
+
+  metric_aggregation_type = "Average"
+}
+
