c49e733decab4a14499e89aaed2beb31b2e979d7
Ankush Maheshwari <ankush97m@gmail.com>
2024-11-16 18:14:33 -0500

    add IAM for lambda

--- iam.tf
+++ iam.tf
@@ -52,3 +52,62 @@ resource "aws_iam_instance_profile" "cloudwatch_instance_profile" {
   name = "CloudWatchInstanceProfile"
   role = aws_iam_role.cloudwatch_role.name
 }
+
+# IAM Role for Lambda
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_email_verification_role"
+  assume_role_policy = jsonencode({
+    Version : "2012-10-17"
+    Statement : [
+      {
+        Action    : "sts:AssumeRole"
+        Effect    : "Allow"
+        Principal : {
+          Service : "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# IAM Policy for Lambda
+resource "aws_iam_policy" "lambda_policy" {
+  name = "lambda_email_verification_policy"
+  policy = jsonencode({
+    Version : "2012-10-17"
+    Statement : [
+      {
+        Action   : [
+          "sns:Publish",
+          "ses:SendEmail",
+          "ses:SendRawEmail"
+        ]
+        Effect   : "Allow"
+        Resource : "*"
+      },
+      {
+        Action   : [
+          "rds:DescribeDBInstances",
+          "rds:Connect"
+        ]
+        Effect   : "Allow"
+        Resource : "*"
+      },
+      {
+        Action   : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   : "Allow"
+        Resource : "*"
+      }
+    ]
+  })
+}
+
+# Attach Policy to IAM Role
+resource "aws_iam_role_policy_attachment" "lambda_role_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
