4a190c57cb57c37dd195979081530adda1d34c03
Ankush Maheshwari <ankush97m@gmail.com>
2024-11-16 23:19:53 -0500

    setup serverless repo

---
+++ utils/db.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres', // Change to 'postgres' for PostgreSQL
+});
+
+const EmailLog = sequelize.define('EmailLog', {
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  verificationLink: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  expirationTime: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+});
+
+module.exports = { sequelize, EmailLog };
