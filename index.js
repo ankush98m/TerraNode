d777a7fd537f752e7b65c5cc654381fd2cb8126c
Ankush Maheshwari <ankush97m@gmail.com>
2024-11-19 07:16:13 -0500

    remove commented code

--- index.js
+++ index.js
@@ -17,54 +17,6 @@ const logger = winston.createLogger({
   ],
 });
 
-// Setup Sequelize for DB connection
-// const sequelize = new Sequelize(
-//   process.env.DB_DATABASE,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: "postgres",
-//     logging: false,
-//   }
-// );
-
-// Sequelize Model for User Verification
-// const EmailLog = sequelize.define(
-//   "EmailLog",
-//   {
-//     id: {
-//       type: DataTypes.UUID,
-//       allowNull: true,
-//       defaultValue: Sequelize.UUIDV4,
-//       primaryKey: true,
-//     },
-//     email: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       unique: true,
-//     },
-//     token: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       unique: true,
-//     },
-//     email_sent_time: {
-//       type: DataTypes.DATE,
-//       defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
-//       allowNull: false,
-//     },
-//     email_verified_time: {
-//       type: DataTypes.DATE,
-//       allowNull: true,
-//     },
-//   },
-//   {
-//     timestamps: false,
-//   }
-// );
-
 // AWS SNS client setup
 const snsClient = new SNSClient({
   region: "us-east-1", // Change to your desired AWS region
@@ -120,16 +72,6 @@ exports.handler = async (event) => {
     logger.info("Data received from SNS:", { email: userData.email });
     console.log("Data received from SNS:", userData.email);
 
-    // Check DB connection
-    // const isDBUp = await checkDBConnection();
-    // if (isDBUp) {
-    //   const token = await createToken(userData);
-    //   if (token) {
-    //     await sendEmail(token, userData.email);
-    //     return { statusCode: 200, body: "Email sent successfully." };
-    //   }
-    // }
-
     await sendEmail(userData.token, userData.email);
     return { statusCode: 200, body: "Email sent successfully." };
     logger.error("Error in processing SNS message", { error: error.message });
@@ -139,41 +81,3 @@ exports.handler = async (event) => {
     return { statusCode: 400, body: JSON.stringify(error) };
   }
 };
-
-// Check DB connection
-// async function checkDBConnection() {
-//   try {
-//     await sequelize.authenticate();
-//     console.log("Database is running");
-//     logger.info("Database connection is healthy.");
-//     return true;
-//   } catch (error) {
-//     logger.error("Database connection failed", { error: error.message });
-//     console.error("Database failed to run", error);
-//     return false;
-//   }
-// }
-
-// Create a new user verification record
-// async function createToken(userDetails) {
-//   try {
-//     // Generate a random 32-byte token (256 bits)
-//     const token = crypto.randomBytes(32).toString("hex");
-
-//     // Store the token in the database or use it as needed
-//     await EmailLog.create({
-//       email: userDetails.email,
-//       token: token,
-//       email_sent_time: new Date(),
-//       email_verified_time: null, // Initially set to null as it hasn't been verified yet
-//     });
-
-//     logger.info("Token created successfully for email:", userDetails.email);
-//     // Return the generated token
-//     return token;
-//   } catch (error) {
-//     logger.error("Error creating token for email:", { email: userDetails.email, error: error.message });
-//     console.error("Error creating token:", error);
-//     return null;
-//   }
-// }
